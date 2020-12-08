c84b6684634d30826f71dedb1ade85bf5423a140
Ariana Rubi <arianarubigo@gmail.com>
2020-12-08 12:11:53 +0100

    hardcoded json added

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,39 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        customer-dashboard
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <h1>Track &amp; trace</h1>
+    <h2>{{ bookingDetails.booking }}</h2>
+    <h2>{{ bookingDetails.shipper }}</h2>
   </div>
 </template>
 
 <script>
-export default {}
+import data from '~/static/bookingData.json'
+
+export default {
+  data() {
+    return {
+      bookingDetails: [],
+    }
+  },
+  asyncData ({ params }) {
+    console.log(data)
+    return {bookingDetails:data}
+  }
+
+  // async asyncData () {
+  //   const {data} = await axios.get('jsonObject')
+  //   console.log('Data:' + data)
+  //   return {data}
+
+  // },
+
+  // async asyncData({ params, $http }) {
+  //     const post = await $http.$get(`https://api.nuxtjs.dev/posts/${params.id}`)
+  //     return { post }
+  //   },
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
