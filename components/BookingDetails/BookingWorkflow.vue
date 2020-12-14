ec3fce1dd1b86108bc2f94677fd7f7fe0bd38ce2
Ariana Rubi <arianarubigo@gmail.com>
2020-12-14 15:34:57 +0100

    workflow section first level architecture

--- components/BookingDetails/BookingWorkflow.vue
+++ components/BookingDetails/BookingWorkflow.vue
@@ -3,7 +3,119 @@
     <b-container class="mt-4">
       <!-- <b-container fluid class="mt-4"> -->
       <h2 class="m-4">Booking Workflow</h2>
+
       <div class="accordion" role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-1 variant="primary">
+          <b-row>
+            <b-col cols="12" class="text-left">
+              statusVerbose 1
+            </b-col>
+            <b-col class="text-left">
+              Accomplished Date:<br>{{ workflow.accomplishedDate }}
+            </b-col>
+          </b-row>
+        </b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-2 variant="primary">
+          <b-row>
+            <b-col cols="12" class="text-left">
+              statusVerbose 2
+            </b-col>
+            <b-col class="text-left">
+              Accomplished Date: {{ workflow.accomplishedDate }}
+            </b-col>
+          </b-row>
+        </b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>
+            <b-table
+                hover
+                :items="items"
+                :fields="fields"
+                responsive="sm"
+              ></b-table>
+              <!-- if statusVerbose has steps, show the plus/details button OR if statusVerbose.steps = Null, don't show details button -->
+          </b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-3 variant="primary">
+          <b-row>
+            <b-col cols="12" class="text-left">
+              statusVerbose 3
+            </b-col>
+            <b-col class="text-left">
+              Accomplished Date: {{ workflow.accomplishedDate }}
+            </b-col>
+          </b-row>
+        </b-button>
+      </b-card-header>
+      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-4 variant="primary">
+          <b-row>
+            <b-col cols="12" class="text-left">
+              statusVerbose 4
+            </b-col>
+            <b-col class="text-left">
+              Accomplished Date: {{ workflow.accomplishedDate }}
+            </b-col>
+          </b-row>
+        </b-button>
+      </b-card-header>
+      <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-5 variant="primary">
+          <b-row>
+            <b-col cols="12" class="text-left">
+              statusVerbose 5
+            </b-col>
+            <b-col class="text-left">
+              Accomplished Date: {{ workflow.accomplishedDate }}
+            </b-col>
+          </b-row>
+        </b-button>
+      </b-card-header>
+      <b-collapse id="accordion-5" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>test</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
+
+      <!-- <div class="accordion" role="tablist">
         <b-card no-body class="mb-1">
           <b-card-header
             header-tag="header"
@@ -48,38 +160,7 @@
             </b-card-body>
           </b-collapse>
         </b-card>
-
-        <b-card no-body class="mb-1">
-          <b-card-header
-            header-tag="header"
-            class="p-1"
-            role="tab"
-            variant="primary"
-          >
-            <b-row class="d-flex align-items-center">
-              <b-col> customs Export </b-col>
-              <b-col> Accomplished Date: 22-Oct-2020 </b-col>
-              <b-button v-b-toggle.accordion-2 class="p-0 mr-4">
-                <b-icon icon="plus-square" aria-label="Show Details"></b-icon>
-              </b-button>
-            </b-row>
-          </b-card-header>
-          <b-collapse
-            id="accordion-2"
-            visible
-            accordion="my-accordion"
-            role="tabpanel"
-          >
-            <b-card-body>
-              <b-table
-                hover
-                :items="tests"
-                responsive="sm"
-              ></b-table>
-            </b-card-body>
-          </b-collapse>
-        </b-card>
-      </div>
+      </div> -->
     </b-container>
   </div>
 </template>
@@ -100,12 +181,6 @@ export default {
         { status_verbose: "Shipping advice", alarm_date: "---" },
       ],
       tests: [{ status_verbose: "test", alarm_date: "---" }],
-
-      // preCarriage: this.bookingData.workflowStatus.preCarriage,
-      // customsExport: this.bookingData.workflowStatus.customsExport,
-      // mainTransit: this.bookingData.workflowStatus.mainTransit,
-      // customsImport: this.bookingData.workflowStatus.customsImport,
-      // onCarriage: this.bookingData.workflowStatus.onCarriage,
     };
   },
 };
