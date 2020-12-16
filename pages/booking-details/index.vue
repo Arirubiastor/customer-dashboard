fcc1d279d6e22c8c122778e31c60e3fde08bb446
Ariana Rubi <arianarubigo@gmail.com>
2020-12-16 17:45:51 +0100

    json parameters updated

--- pages/booking-details/index.vue
+++ pages/booking-details/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center">Booking Details</h1>
     <BookingHeader :bookingData="bookingData" />
     <BookingStatus :bookingData="bookingData" />
-    <!-- <BookingWorkflow :workflow="bookingData.workflow" /> -->
+    <BookingWorkflow :workflow="bookingData.workflow" />
     <b-button tag="nuxt-link" to="/" variant="outline-primary"
       >Back to login</b-button
     >
@@ -24,7 +24,7 @@ export default {
   async created() {
     // GET request using fetch with async/await
     const response = await fetch(
-      "https://e7pmpg7z85.execute-api.us-west-2.amazonaws.com/prod/obtienedatosbooking?identityBooking=119524"
+      "https://e7pmpg7z85.execute-api.us-west-2.amazonaws.com/prod/obtienedatosbooking?identityBooking=46940&nombreCliente=Schryver"
     );
     const data = await response.json();
     console.log((JSON.parse(data.JsonBooking)))
