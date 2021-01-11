5db2bd0879b2499801b60629fa651df5c1160318
Ariana Rubi <arianarubigo@gmail.com>
2021-01-11 20:54:08 +0100

    styles

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <b-navbar type="dark" variant="primary">
-      <b-navbar-brand>Brand</b-navbar-brand>
-      <b-navbar-nav>
-        <b-navbar-text class="text-light">Track &amp; trace</b-navbar-text>
-      </b-navbar-nav>
+      <b-navbar-brand class="ml-md-4">Brand</b-navbar-brand>
+      <b-nav-text class="ml-auto mr-md-4">
+        <b-navbar-text class="text-light font-weight-bold">Track &amp; Trace</b-navbar-text>
+      </b-nav-text>
     </b-navbar>
   </div>
 </template>
