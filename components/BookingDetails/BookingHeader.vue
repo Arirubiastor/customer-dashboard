2992e873d0b97d55111fabf922d4fd37deccd5c2
Ariana Rubi <arianarubigo@gmail.com>
2021-01-11 20:56:15 +0100

    icons added

--- components/BookingDetails/BookingHeader.vue
+++ components/BookingDetails/BookingHeader.vue
@@ -2,24 +2,48 @@
   <div>
     <b-container>
       <b-card-group deck>
-      <b-card no-body class="p-2" border-variant="primary" align="center">
-        <b-card-header class="pt-1 pt-md-2">Booking</b-card-header>
-        <p class="font-weight-bold mb-2 mt-2 mt-md-3">
-          {{ bookingData.booking }}
-        </p>
-      </b-card>
-      <b-card no-body class="p-2" border-variant="primary" align="center">
-        <b-card-header class="pt-1 pt-md-2">Shipper</b-card-header>
-        <p class="font-weight-bold mb-2 mt-2 mt-md-3">
-          {{ bookingData.shipper }}
-        </p>
-      </b-card>
-      <b-card no-body class="p-2" border-variant="primary" align="center">
-        <b-card-header class="pt-1 pt-md-2">Consignee</b-card-header>
-        <p class="font-weight-bold mb-2 mt-2 mt-md-3">
-          {{ bookingData.consignee }}
-        </p>
-      </b-card>
+        <b-card no-body class="p-2" border-variant="primary" align="center">
+          <b-card-header class="pt-1 pt-md-2 d-inline">
+          <b-icon
+            icon="box"
+            variant="primary"
+            style="width: 20px; height: 20px"
+            class="d-inline mr-2"
+          ></b-icon>
+            Booking
+          </b-card-header>
+          <p class="font-weight-bold mb-2 mt-2 mt-md-3">
+            {{ bookingData.booking }}
+          </p>
+        </b-card>
+        <b-card no-body class="p-2" border-variant="primary" align="center">
+          <b-card-header class="pt-1 pt-md-2">
+            <b-icon
+            icon="box-arrow-right"
+            variant="primary"
+            style="width: 20px; height: 20px"
+            class="d-inline mr-2"
+          ></b-icon>
+            Shipper
+          </b-card-header>
+          <p class="font-weight-bold mb-2 mt-2 mt-md-3">
+            {{ bookingData.shipper }}
+          </p>
+        </b-card>
+        <b-card no-body class="p-2" border-variant="primary" align="center">
+          <b-card-header class="pt-1 pt-md-2">
+            <b-icon
+            icon="box-arrow-in-right"
+            variant="primary"
+            style="width: 20px; height: 20px"
+            class="d-inline mr-2"
+          ></b-icon>
+            Consignee
+          </b-card-header>
+          <p class="font-weight-bold mb-2 mt-2 mt-md-3">
+            {{ bookingData.consignee }}
+          </p>
+        </b-card>
       </b-card-group>
     </b-container>
   </div>
@@ -31,12 +55,10 @@ export default {
     bookingData: {
       type: Object,
       required: true,
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-
-
 </style>
